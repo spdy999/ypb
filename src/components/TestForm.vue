c9ee9a3bd9fea3dc0efedf1957f39c5dfc75696d
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 21:37:02 +0700

    edit TestForm

--- src/components/TestForm.vue
+++ src/components/TestForm.vue
@@ -1,54 +1,43 @@
 <template>
-  <form id="app" data-apartments="initial">
-    <h1>Dynamic apartment forms</h1>
-    <hr />
-    <div class="row">
-      <div class="col-xs-2">
-        <button type="button" v-on:click="addNewApartment" class="btn btn-block btn-success">Add +</button>
-      </div>
-      <div class="col-xs-10">Would you like add more apartments?</div>
-    </div>
-    <div v-for="(apartment, index) in apartments" :key="index">
-      <div class="row">
-        <div class="col-xs-2">
-          <label>&nbsp;</label>
-          <button
-            type="button"
-            v-on:click="removeApartment(index)"
-            class="btn btn-block btn-danger"
-          >Rem -</button>
-        </div>
+  <form id="app" data-customers="initial">
+    <div v-for="(customer, index) in customers" :key="index">
+      <div>
         <div class="form-group col-xs-5">
-          <label>Price (HUF)</label>
+          <label>Name</label>
           <input
-            v-model="apartment.price"
-            type="number"
-            name="apartments[][price]"
+            v-model="customer.name"
+            name="customers[][name]"
             class="form-control"
-            placeholder="Price"
+            placeholder="Name"
+            required
           />
         </div>
         <div class="form-group col-xs-5">
-          <label>Rooms (PCS)</label>
+          <label>Email</label>
           <input
-            v-model="apartment.rooms"
-            type="number"
-            name="apartments[][rooms]"
+            v-model="customer.email"
+            type="email"
+            name="customers[][email]"
             class="form-control"
-            placeholder="Rooms"
+            placeholder="Email"
           />
         </div>
       </div>
     </div>
+
     <div class="row">
       <div class="col-xs-2">
         <button
-          type="submit"
-          v-on:click.prevent="sumbitForm"
-          class="btn btn-block btn-primary"
-        >Submit</button>
+          type="button"
+          v-on:click="addCustomer"
+          class="btn btn-block btn-success"
+        >Add Customer</button>
       </div>
-      <div class="col-xs-10">Open the console (F12) and see the result</div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2">
+        <button type="submit" v-on:click.prevent="sumbitForm" class="btn btn-block btn-primary">Save</button>
+      </div>
     </div>
     <hr />
     <pre>{{ $data }}</pre>
@@ -60,40 +49,30 @@ export default {
   data() {
     return {
       initial: [
-        { price: "23000", rooms: "12" },
-        { price: "42000", rooms: "32" }
+        { name: "Name", email: "email@mail.com" }
+        // { name: "42000", email: "32" }
       ],
-      apartment: {
-        price: "",
-        rooms: ""
+      customer: {
+        name: "",
+        email: ""
       },
-      apartments: []
+      customers: []
     };
   },
   mounted() {
-    /*
-     * The "data-apartments" could come from serverside (already saved apartments)
-     */
-    // this.apartments = JSON.parse(this.$el.dataset.apartments);
-    this.apartments = this.initial;
+    this.customers = this.initial;
   },
   methods: {
-    addNewApartment() {
-      this.apartments.push(this.apartment);
+    addCustomer() {
+      this.customers.push(this.customer);
     },
     removeApartment() {
       // removeApartment(index) {
-      // Vue.delete(this.apartments, index);
+      // Vue.delete(this.customers, index);
     },
     sumbitForm() {
-      /*
-       * You can remove or replace the "submitForm" method.
-       * Remove: if you handle form sumission on server side.
-       * Replace: for example you need an AJAX submission.
-       */
       console.info("<< Form Submitted >>");
-      console.info("Vue.js apartments object:", this.apartments);
-      window.testSumbit();
+      console.info("Vue.js customers object:", this.customers);
     }
   }
 };
