415a04421c78f0d09ea958152571365962b53331
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 22:58:19 +0700

    remove unused components

--- src/components/MultipleCustomerInformationRecording.vue
+++ src/components/MultipleCustomerInformationRecording.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <!-- <b-button @click="onClickAddCustomer">Add Customer</b-button>
-    <b-button>Save</b-button>-->
-    <!-- <TestForm /> -->
     <CustomerForm />
   </div>
 </template>
 
 <script>
-// import TestForm from "./TestForm.vue";
 import CustomerForm from "./CustomerForm.vue";
 export default {
   components: {
-    // TestForm,
     CustomerForm
   },
   methods: {
--- src/components/ResultModal.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <b-modal id="modal-1" title="BootstrapVue">
-    <p class="my-4">Hello from modal!</p>
-  </b-modal>
-</template>
