0f48268da78d03915858760fae79cbf51bd3e35f
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 20:38:30 +0700

    route to CustomerForm

--- src/components/MultipleCustomerInformationRecording.vue
+++ src/components/MultipleCustomerInformationRecording.vue
@@ -2,20 +2,14 @@
   <div>
     <b-button @click="onClickAddCustomer">Add Customer</b-button>
     <b-button>Save</b-button>
-    <CustomerForm />
   </div>
 </template>
 
 <script>
-import CustomerForm from "./CustomerForm";
-
 export default {
-  components: {
-    CustomerForm
-  },
   methods: {
-    onClickAddCustomer: () => {
-      console.log("hello");
+    onClickAddCustomer() {
+      this.$router.push({ path: "/CustomerForm" });
     }
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import MultipleCustomerInformationRecording from './components/MultipleCustomerInformationRecording.vue';
+import CustomerForm from './components/CustomerForm.vue';
 
-const routes = [{ path: '/', component: MultipleCustomerInformationRecording }];
+const routes = [
+  { path: '/', component: MultipleCustomerInformationRecording },
+  { path: '/CustomerForm', component: CustomerForm },
+];
 
 export default routes;
