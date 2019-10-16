da9afd54b070ca357282b0dc45837cc0b5dae134
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-16 19:51:43 +0700

    add form

--- src/components/MultipleCustomerInformationRecording.vue
+++ src/components/MultipleCustomerInformationRecording.vue
@@ -2,5 +2,19 @@
   <div>
     <b-button>Add Customer</b-button>
     <b-button>Save</b-button>
+    <CustomerForm />
   </div>
 </template>
+
+<script>
+import CustomerForm from "./CustomerForm";
+
+export default {
+  components: {
+    CustomerForm
+  }
+  // methods: {
+  //   onClickAddCustomer() {}
+  // }
+};
+</script>
